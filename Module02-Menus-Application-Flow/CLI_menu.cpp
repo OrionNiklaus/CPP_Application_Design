672ebe2909c8f7b8c21b6a93ad3f314a26197642
OrionNiklaus <orionv@berkeley.edu>
2026-09-07 15:22:51 -0700

    finished module 3.

--- Module02-Menus-Application-Flow/CLI_menu.cpp
+++ Module02-Menus-Application-Flow/CLI_menu.cpp
@@ -39,52 +39,84 @@ void strafe_gait(int sec) {
 
 int main() {
 
+    double version=1.0;
+    string name="default";
+    string appName="Hexapod Control Application";
     int choice = 0;
     int sec = 0;
+    double speed = 0.0;
+    string dir = "up";
 
-    cout << "--Hexapod Control Application--" << endl;
+    // change motors_ready to test both cases
+    
+    bool motors_ready = true;
+ 
+    cout << "Please enter your name: ";
+    cin >> name;
+    cout << " ----- " << endl;
+
+    cout << "Welcome to the Application," << name << "!" << endl;
+    cout << "Application Name: " << appName << endl;
+    cout << "Vesion Number: " << version << endl;
+    cout << " ----- " << endl;
+
     cout << "Please enter program runtime in seconds: ";
     cin >> sec;
-    cout << " --- " << endl;
+    cout << " ----- " << endl;
 
-    while (choice != 6) {
-        cout << "Please select the gait pattern:" << endl;
-        cout << "1. Standard gait pattern." << endl;
-        cout << "2. Wave gait pattern." << endl;
-        cout << "3. Reverse gait pattern." << endl;
-        cout << "4. Dashing gait pattern." << endl;
-        cout << "5. Strafing gait pattern." << endl;
-        cout << "6. Exit application." << endl;
-        cout << " --- " << endl;
-        cout << "Please enter your selection: ";
-        cin >> choice;
-        cout << " " << endl;
-        cout << " --- " << endl;
+    cout << "Last Movement Direction: " << dir << endl;
+    cout << "Current Speed: " << to_string(speed) << " (m/s)" << endl; 
+    
+    if (motors_ready) {
+        cout << "Motors armed." << endl;
+    }
+    
+    while (motors_ready) {
+        while (choice != 6) {
+            cout << "Please select the gait pattern:" << endl;
+            cout << "1. Standard gait pattern." << endl;
+            cout << "2. Wave gait pattern." << endl;
+            cout << "3. Reverse gait pattern." << endl;
+            cout << "4. Dashing gait pattern." << endl;
+            cout << "5. Strafing gait pattern." << endl;
+            cout << "6. Exit application." << endl;
+            cout << " --- " << endl;
+            cout << "Please enter your selection: ";
+            cin >> choice;
+            cout << " " << endl;
+            cout << " --- " << endl;
 
-        switch (choice) {
-            case 1:
-                std_gait(sec);
-                break;
-            case 2:
-                wave_gait(sec);
-                break;
-            case 3:
-                rev_gait(sec);
-                break;
-            case 4:
-                dash_gait(sec);
-                break;
-            case 5:
-                strafe_gait(sec);
-                break;
-            case 6:
-                cout << "See you next time!" << endl;
-                break;
-            default:
-                cout << "Please select a valid option." << endl;
-        }   
+            switch (choice) {
+                case 1:
+                    std_gait(sec);
+                    break;
+                case 2:
+                    wave_gait(sec);
+                    break;
+                case 3:
+                    rev_gait(sec);
+                    break;
+                case 4:
+                    dash_gait(sec);
+                    break;
+                case 5:
+                    strafe_gait(sec);
+                    break;
+                case 6:
+                    cout << "See you next time!" << endl;
+                    break;
+                default:
+                    cout << "Please select a valid option." << endl;
+            }   
+        
+        } 
+
+    }
+
+    if (!motors_ready) {
+        cout << "Motors disabled." << endl;
+        cout << "See troubleshooting documentation." << endl;
     }
 
     return 0;
-
 }
--- Module03-Variables/first_application.cpp
+++ Module03-Variables/first_application.cpp
@@ -3,10 +3,20 @@
 using namespace std;
 
 int main() {
+
     double version=1.0;
     string name="default";
     string appName="Hexapod Control Application";
-    
+
+    int choice = 0;
+    int sec = 0;
+    double speed = 0.0;
+    string dir = "up";
+
+    // change motors_ready to test both cases
+
+    bool motors_ready = true;
+
     cout << "Please enter your name: ";
     cin >> name;
     cout << " ----- " << endl;
@@ -14,5 +24,22 @@ int main() {
     cout << "Welcome to the Application, " << name << "!" << endl;
     cout << "Application Name: " << appName << endl;
     cout << "version Number: " << version << endl;
+    cout << " ----- " << endl;
+
+    cout << "Please enter program runtime in seconds: ";
+    cin >> sec;
+    cout << " ----- " << endl;
+
+    cout << "Last Movement Direction: " << dir << endl;
+    cout << "Current Speed: " << to_string(speed) << " (m/s)" << endl;
+
+    cout << "Motors Ready? -- ";
+    
+    if (motors_ready) {
+        cout << "Motors armed." << endl;
+    } else {
+        cout << "Motors disabled." << endl;
+        cout << "See troubleshooting documentation." << endl;
+    }
 
 }
